79c08d5282cb49944ff63b72d54ac4ad9e36be55
StefanoMilani <milani@diag.uniroma1.it>
2023-09-16 17:38:37 +0200

    FIX Aggregation function and send routine
    Modified proto file
    
    Signed-Off-By: Stefano Milani <milani@diag.uniroma1.it>

--- src/e2l_crypto/e2l_crypto.rs
+++ src/e2l_crypto/e2l_crypto.rs
@@ -1,4 +1,8 @@
 use self::e2l_crypto::E2LCrypto;
+static AVG_ID: u8 = 1;
+static SUM_ID: u8 = 2;
+static MIN_ID: u8 = 3;
+static MAX_ID: u8 = 4;
 pub(crate) mod e2l_crypto {
     // Crypto
     extern crate p256;
@@ -18,12 +22,25 @@ pub(crate) mod e2l_crypto {
     use sha2::Digest;
     use sha2::Sha256;
 
+    static AVG_ID: u8 = 1;
+    static SUM_ID: u8 = 2;
+    static MIN_ID: u8 = 3;
+    static MAX_ID: u8 = 4;
+
+    pub struct AggregationResult {
+        pub status_code: i64,
+        pub dev_eui: String,
+        pub dev_addr: String,
+        pub aggregated_data: i64,
+    }
+
     pub struct DevInfo {
         dev_eui: String,
         dev_addr: String,
         dev_public_key: P256PublicKey<p256::NistP256>,
         edge_s_enc_key: AES128,
         edge_s_int_key: AES128,
+        values: Vec<i64>,
     }
 
     #[derive(Default)]
@@ -32,6 +49,8 @@ pub(crate) mod e2l_crypto {
         pub public_key: Option<P256PublicKey<p256::NistP256>>,
         pub compressed_public_key: Option<Box<[u8]>>,
         pub active_directory: Vec<DevInfo>,
+        pub aggregation_function: u8,
+        pub window_size: usize,
     }
 
     impl E2LCrypto {
@@ -53,6 +72,7 @@ pub(crate) mod e2l_crypto {
             let result_projective_point = non_identity_point.mul(*non_zero_scalar);
             return P256PublicKey::from_affine(result_projective_point.to_affine());
         }
+
         /*
            @brief: This function computes the private/public ecc key pair of the GW
            @return: the compressed public key of the GW to be sent to the AS
@@ -139,7 +159,9 @@ pub(crate) mod e2l_crypto {
             // Add Info to dev info struct
             let mut dev_info_found = false;
             for dev_info in self.active_directory.iter_mut() {
-                if dev_info.dev_addr == dev_addr {
+                if dev_info.dev_eui == dev_eui {
+                    // if dev_info.dev_addr == dev_addr {
+                    dev_info.dev_addr = dev_addr.clone();
                     dev_info.dev_public_key = dev_public_key.clone();
                     dev_info.edge_s_enc_key = edge_s_enc_key;
                     dev_info.edge_s_int_key = edge_s_int_key;
@@ -154,9 +176,11 @@ pub(crate) mod e2l_crypto {
                     dev_public_key: dev_public_key.clone(),
                     edge_s_enc_key: edge_s_enc_key,
                     edge_s_int_key: edge_s_int_key,
+                    values: Vec::new(),
                 };
                 self.active_directory.push(new_dev_info);
             }
+            println!("Added dev addr: {:?} to active directory.", dev_addr);
 
             let g_gw_ed = self
                 .scalar_point_multiplication(self.private_key.clone().unwrap(), dev_public_key)
@@ -172,6 +196,7 @@ pub(crate) mod e2l_crypto {
         pub fn check_e2ed_enabled(&self, dev_addr: String) -> bool {
             for dev_info in self.active_directory.iter() {
                 if dev_info.dev_addr == dev_addr {
+                    println!("Device is in the active directory");
                     return true;
                 }
             }
@@ -183,43 +208,81 @@ pub(crate) mod e2l_crypto {
            @param dev_addr: the dev_addr of the device
            @param fcnt: the frame counter
            @param phy: the encrypted data payload
-           @return: 0 if the frame was processed, -1 otherwise
+           @return: None if aggregated function not defined or device not found (should never happen), an AggregatedResult Structure if success.
+           @status_code: 0 if aggregated result if returned and need to send it to DM, -1 otherwise.
         */
         pub fn process_frame(
-            &self,
+            &mut self,
             dev_addr: String,
             fcnt: u16,
             phy: EncryptedDataPayload<Vec<u8>, DefaultFactory>,
-        ) -> i32 {
-            let dev_info: &DevInfo;
+        ) -> Option<AggregationResult> {
+            let dev_info: &mut DevInfo;
             let mut dev_info_found = false;
-            for dev_info_iter in self.active_directory.iter() {
+            for dev_info_iter in self.active_directory.iter_mut() {
                 if dev_info_iter.dev_addr == dev_addr {
                     dev_info = dev_info_iter;
-                    println!("Dev info {}", dev_info.dev_addr);
                     dev_info_found = true;
                     // GET KEYS
                     let edge_s_enc_key: AES128 = dev_info.edge_s_enc_key.clone();
-                    let edge_s_int_key: AES128 = dev_info.edge_s_int_key.clone();
+                    // let edge_s_int_key: AES128 = dev_info.edge_s_int_key.clone();
                     let decrypted_data_payload = phy
-                        .decrypt(Some(&edge_s_enc_key), Some(&edge_s_int_key), fcnt.into())
+                        .decrypt(Some(&edge_s_enc_key), Some(&edge_s_enc_key), fcnt.into())
                         .unwrap();
 
                     let frame_payload_result = decrypted_data_payload.frm_payload().unwrap();
                     match frame_payload_result {
                         lorawan_encoding::parser::FRMPayload::Data(frame_payload) => {
-                            println!("Edge Frame Payload: {:?}", frame_payload);
+                            let frame_payload_vec: Vec<u8> = frame_payload.to_vec();
+                            let sensor_value: i64 = frame_payload_vec[0].into();
+                            println!("Edge Frame Payload: {:?}\n", sensor_value);
+                            dev_info.values.push(sensor_value);
+                            if dev_info.values.len() >= self.window_size {
+                                let aggregation_result: i64;
+                                match self.aggregation_function {
+                                    i if i == AVG_ID => {
+                                        let sum: i64 = dev_info.values.iter().sum();
+                                        aggregation_result = sum / (dev_info.values.len() as i64);
+                                    }
+                                    i if i == SUM_ID => {
+                                        aggregation_result = dev_info.values.iter().sum();
+                                    }
+                                    i if i == MIN_ID => {
+                                        aggregation_result = *dev_info.values.iter().min().unwrap();
+                                    }
+                                    i if i == MAX_ID => {
+                                        aggregation_result = *dev_info.values.iter().max().unwrap();
+                                    }
+                                    _ => {
+                                        println!("Aggregation function not supported!");
+                                        return None;
+                                    }
+                                }
+                                println!("\n\nValues: {:?}", dev_info.values);
+                                println!("Aggregation result: {:?}\n\n", aggregation_result);
+                                dev_info.values = Vec::new();
+
+                                return Some(AggregationResult {
+                                    status_code: 0,
+                                    dev_eui: dev_info.dev_eui.clone(),
+                                    dev_addr: dev_info.dev_addr.clone(),
+                                    aggregated_data: aggregation_result,
+                                });
+                            }
                         }
-                        _ => println!("Failed to decrypt packet"),
+                        _ => {
+                            println!("Failed to decrypt packet");
+                            return None;
+                        }
                     };
                     break;
                 }
             }
-            if dev_info_found {
-                return -1;
+            if !dev_info_found {
+                return None;
             }
 
-            return 0;
+            return None;
         }
     }
 }
@@ -229,4 +292,6 @@ pub static mut E2L_CRYPTO: E2LCrypto = E2LCrypto {
     public_key: None,
     compressed_public_key: None,
     active_directory: Vec::new(),
+    aggregation_function: AVG_ID,
+    window_size: 5,
 };
